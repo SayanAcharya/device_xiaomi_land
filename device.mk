dbdecefd42b6714327671c096d07e64be861089c
Ritesh Saxena <riteshsax007@gmail.com>
2018-01-28 12:09:10 +0000

    land: Switch to OSS RIL
    Change-Id: Ic49d4983e3fd5a78ffad586c2e92e0efcb67acb0

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2 \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full \
+    libqdMetaData.system
 
 PRODUCT_PACKAGES += \
     telephony-ext
